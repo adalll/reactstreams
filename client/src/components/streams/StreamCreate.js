aae08b7f32d6a66c60919ecb593f339d2ec22d6a
adalll <adalll@mail.ru>
2019-07-01 18:33:33 +0700

    Work 1 Client + Server After Making Actions and stream Reducer

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from 'react-redux';
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderInput = formProps => {
@@ -25,7 +27,9 @@ class StreamCreate extends React.Component {
     }
   }
 
-  onSubmit(formValues) {}
+  onSubmit = (formValues) => {
+      this.props.createStream(formValues);
+  }
 
   render() {
     return (
@@ -58,7 +62,9 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate: validate
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
